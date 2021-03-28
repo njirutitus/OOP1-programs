72e208edba8fa96dbc53cce5f07d5cd43d18a721
titus njiru <njirutitus@gmail.com>
2021-03-28 23:45:30 +0300

    Inheritance - Employee class

--- Employee/main.cpp
+++ Employee/main.cpp
@@ -1,3 +1,8 @@
+/******************************************
+* A program to track details of Employees *
+* Written by Titus Njiru                  *
+* March 2021                              *
+******************************************/
 #include <iostream>
 
 using namespace std;
@@ -8,74 +13,125 @@ class Employee {
     int id;
     string name;
     float salary;
+    bool is_male;
+    bool is_married;
+
 
     // public member functions declaration
 public:
-    Employee();
-    Employee(const int i, const string n, const float s);
-    void set_employee(const int i, const string n, const float s);
+    Employee(); // default constructor
+    Employee(const int i, const string n, const float s,const bool male, const bool married); // parameterized constructor
+    void set_employee(const int i, const string n, const float s, const bool is_male, const bool is_married);
     void get_employee();
 };
 
-// constructor definition
-// Used to set initial values for the employe
-// doesn't take any argumes
+// Default constructor definition
+// Used to set initial values for the employee
+// does n't take any arguments
 
 Employee::Employee(){
    id = 0;
    name = "No name";
    salary = 0;
 }
+/////////////////////////////////////////////////////////
+// parameterized constructor definition                //
+// used to initialize an object with the given values  //
+// requires 3 arguments                                //
+/////////////////////////////////////////////////////////
 
-// parameterized constructor definition
-// used to initialize an object with the given values
-// requires 3 arguments
-
-
-Employee::Employee(const int i, const string n, const float s) {
+Employee::Employee(const int i, const string n, const float s,const bool male, const bool married) {
     id = i;
     name = n;
     salary = s;
+    is_male = male;
+    is_married = married;
 }
 
-// definition of set_employee member function
+// definition of set employee member function
 // used to set the details of the employee
 // requires 3 arguments
 
-void Employee::set_employee(const int i, const string n, const float s){
+void Employee::set_employee(const int i, const string n, const float s,const bool male, const bool married){
     id = i;
     name = n;
     salary = s;
+    is_male = male;
+    is_married = married;
 }
 
-// definition of get_employee member function
+// definition of get employee member function
 // used to output the details of an employee
-// doesn't take any arguments
+// Does not take any arguments
 
 void Employee::get_employee() {
     cout << "Employee ID: " << id << endl;
     cout << "Employee Name: " << name << endl;
     cout << "Employee Salary: " << salary << endl;
+    string gender = is_male ? "Male" : "Female" ;
+    cout << "Gender: " << gender << endl;
+    string marital_status = is_married ? "Married" : "Single";
+    cout << "Marital status: " << marital_status << endl;
 }
 
+// supervisor class
+class supervisor:public Employee {
+    int sp_allowance;
+public:
+    int get_allowance();
+    void set_allowance(int allowance);
+};
 
+int supervisor::get_allowance() {
+    return sp_allowance;
+}
+
+void supervisor::set_allowance(const int allowance){
+    sp_allowance = allowance;
+}
+
+// Hierarchical inheritance
+// manager inherits from both employee and supervisor
+class manager:public supervisor{
+    int tr_allowance;
+    int hs_allowance;
+public:
+    int get_tr_allowance();
+    void set_tr_allowance(int tr);
+    int get_hs_allowance();
+    void set_hs_allowance(int hs);
+
+};
+
+int manager::get_tr_allowance() {
+    return tr_allowance;
+}
+void manager::set_tr_allowance(int tr){
+    tr_allowance = tr;
+}
+int manager::get_hs_allowance() {
+    return hs_allowance;
+}
+void manager::set_hs_allowance(int hs) {
+    hs_allowance = hs;
+}
 
 int main()
 {
+    supervisor sp1;
+    sp1.set_employee(2001,"Steve Jobs",2000000,1,1);
+    sp1.set_allowance(200000);
 
-    Employee emp1,emp2,emp3,emp4,emp5(1000,"Employee 5", 1000);
-    Employee emp6(1000,"Employee 6", 1000); // Object creation
-    emp1.set_employee(1002, "Bill Gates", 20000000);
-    emp2.set_employee(1003, "Steve Jobs", 30000000);
-    emp3.set_employee(1004, "Elon Musk", 10000000);
-    emp6.set_employee(1006, "Eric mutua", 500000);
+    manager mg1;
+    mg1.set_employee(2002,"Elon Musk",5000000,1,1);
+    mg1.set_allowance(500000);
+    mg1.set_tr_allowance(200000);
+    mg1.set_hs_allowance(100000);
 
-    emp1.get_employee();
-    emp2.get_employee();
-    emp3.get_employee();
-    emp4.get_employee();
-    emp5.get_employee();
-    emp6.get_employee();
+    mg1.get_employee();
+    cout << "Supervisor allowance: " << mg1.get_allowance() << endl;
+    cout << "Travel Allowance: " << mg1.get_tr_allowance() << endl;
+    cout << "House allowance: " << mg1.get_hs_allowance() << endl;
 
     return 0;
 }
